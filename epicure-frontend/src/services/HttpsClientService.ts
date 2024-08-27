5329e29f80aef0195050c8bb3f8178674ff5fc30
Daniela Segal <danielas@moveo.co.il>
2024-08-27 11:02:30 +0300

    fix and add http client

--- epicure-frontend/src/services/HttpsClientService.ts
+++ epicure-frontend/src/services/HttpsClientService.ts
@@ -1,29 +1,27 @@
 import axios from "axios";
 
-const BASE_URL = "/api";
-
 class HttpClient {
   static async getAll(resource: string) {
-    const response = await axios.get(`${BASE_URL}/${resource}`);
+    const response = await axios.get(resource);
     return response.data;
   }
 
   static async get(resource: string, id: string) {
-    const response = await axios.get(`${BASE_URL}/${resource}/${id}`);
+    const response = await axios.get(`${resource}/${id}`);
     return response.data;
   }
 
   static async add(resource: string, item: any) {
-    const response = await axios.post(`${BASE_URL}/${resource}`, item);
+    const response = await axios.post(`${resource}`, item);
     return response.data;
   }
   static async update(resource: string, id: string, item: any) {
-    const response = await axios.put(`${BASE_URL}/${resource}/${id}`, item);
+    const response = await axios.put(`${resource}/${id}`, item);
     return response.data;
   }
 
   static async delete(resource: string, id: string) {
-    const response = await axios.delete(`${BASE_URL}/${resource}/${id}`);
+    const response = await axios.delete(`${resource}/${id}`);
     return response.data;
   }
 }
