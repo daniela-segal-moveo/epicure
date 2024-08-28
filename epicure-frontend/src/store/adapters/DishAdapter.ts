d92f980789501f6514ce1d0c969b840370677036
Daniela Segal <danielas@moveo.co.il>
2024-08-28 11:09:37 +0300

    resturant slice

--- epicure-frontend/src/store/adapters/DishAdapter.ts
+++ epicure-frontend/src/store/adapters/DishAdapter.ts
@@ -1,40 +1,40 @@
-import axios from "axios";
 import { Dish } from "../models/dish.model";
+import HttpClient from "../../services/HttpsClientService";
 
 class DishesAdapter {
   static readonly endpoint = {
     getDishes: "/api/dishes/",
-    getDishById: "/api/dishes/getDish",
-    updateDish: "/api/dishes/updateDish",
-    deleteDish: "/api/dishes/deleteDish",
-    addDish: "/api/dishes/addDish",
+    getDishById: "/api/dishes/get",
+    updateDish: "/api/dishes/update",
+    deleteDish: "/api/dishes/delete",
+    addDish: "/api/dishes/add",
   };
 
   static async getAllDishes(): Promise<Dish[]> {
-    const res = await axios.get(`${DishesAdapter.endpoint.getDishes}`);
+    const res = await HttpClient.getAll(DishesAdapter.endpoint.getDishes);
     return res.data as Dish[];
   }
 
   static async getDish(id: string): Promise<Dish> {
-    const res = await axios.get(`${DishesAdapter.endpoint.getDishById}/${id}`);
+    const res = await HttpClient.get(DishesAdapter.endpoint.getDishById,id);
     return res.data as Dish;
   }
 
   static async addDish(newDish: Dish): Promise<Dish> {
-    const res = await axios.post(`${DishesAdapter.endpoint.addDish}`, newDish);
+    const res = await HttpClient.add(DishesAdapter.endpoint.addDish, newDish);
     return res.data as Dish;
   }
 
-  static async updateDish(updatedChef: Dish): Promise<Dish> {
-    const res = await axios.put(
-      `${DishesAdapter.endpoint.updateDish}`,
-      updatedChef
+  static async updateDish(updatedDish: Dish): Promise<Dish> {
+    const res = await HttpClient.update(DishesAdapter.endpoint.updateDish,updatedDish.id,
+      updatedDish
     );
     return res.data as Dish;
   }
 
-  static async deleteDish(id: string): Promise<void> {
-    await axios.delete(`${DishesAdapter.endpoint.deleteDish}/${id}`);
+  static async deleteDish(id: string): Promise<Dish> {
+    const res = await HttpClient.delete(DishesAdapter.endpoint.deleteDish,id);
+    return res.data as Dish;
   }
 }
 
